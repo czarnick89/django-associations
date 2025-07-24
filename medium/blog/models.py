b9a0e5eed813d783bce31f019578af824387163d
czarnick89 <nso83@hotmail.com>
2025-07-24 16:15:30 -0500

    release01 medium complete

--- medium/blog/models.py
+++ medium/blog/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    
+    @property
+    def posts(self):
+        return Post.objects.filter(author=self)
+    
+    @property
+    def comments(self):
+        return Comment.objects.filter(author=self)
+
+class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    @property
+    def comments(self):
+        return Comment.objects.filter(post=self)
+
+class Comment(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comment')
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comment')
