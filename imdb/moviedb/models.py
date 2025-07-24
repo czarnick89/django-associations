f9698c455f7405bd8926e2c588e74d186a96f885
czarnick89 <nso83@hotmail.com>
2025-07-24 15:45:24 -0500

    release0 done

--- imdb/moviedb/models.py
+++ imdb/moviedb/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Actor(models.Model):
+    @property
+    def movies(self):
+        return Movie.objects.filter(roles__actor=self)
+
+class Movie(models.Model):
+    @property
+    def actors(self):
+        return Actor.objects.filter(roles__movie=self)
+
+class Role(models.Model):
+    actor = models.ForeignKey(Actor, on_delete=models.CASCADE, related_name='roles')
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE, related_name='roles')
+
